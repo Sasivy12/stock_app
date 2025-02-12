8eef654f93c038a0c7041705cb76149e8203496b
Sasivy12 <muslimov.tim05@gmail.com>
2025-02-12 16:42:59 +0300

    made documentation for TransactionController

--- src/main/java/com/example/stock_app/controller/TransactionController.java
+++ src/main/java/com/example/stock_app/controller/TransactionController.java
@@ -1,9 +1,13 @@
 package com.example.stock_app.controller;
 
+import com.example.stock_app.dto.StockDTO;
 import com.example.stock_app.service.TransactionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +23,15 @@ public class TransactionController
 {
     private final TransactionService transactionService;
 
-    @PostMapping("/addfunds/{userId}")
     @Operation(summary = "Add funds", description = "Increases user's balance")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The transaction is successful"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - JWT token required or Invalid request"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PostMapping("/addfunds/{userId}")
+    @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<String> addFunds(@PathVariable("userId") Long userId, @RequestBody Map<String, Double> request)
     {
         double amount = request.get("amount");
