df2ec4dc425a22718da89a8281382c5472f2d48f
Sasivy12 <muslimov.tim05@gmail.com>
2025-02-12 16:37:53 +0300

    updated addFunds method in TransactionController

--- src/main/java/com/example/stock_app/controller/TransactionController.java
+++ src/main/java/com/example/stock_app/controller/TransactionController.java
@@ -1,23 +1,29 @@
 package com.example.stock_app.controller;
 
 import com.example.stock_app.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/")
+@Tag(name = "Transaction Management", description = "API is user for adding funds to user's balance")
 public class TransactionController
 {
     private final TransactionService transactionService;
 
-    @PostMapping("/addfunds")
-    public ResponseEntity<String> addFunds(@RequestParam Long userId, @RequestParam double amount)
+    @PostMapping("/addfunds/{userId}")
+    @Operation(summary = "Add funds", description = "Increases user's balance")
+    public ResponseEntity<String> addFunds(@PathVariable("userId") Long userId, @RequestBody Map<String, Double> request)
     {
+        double amount = request.get("amount");
         transactionService.addFunds(userId, amount);
         return ResponseEntity.ok("The transaction is successful");
     }
